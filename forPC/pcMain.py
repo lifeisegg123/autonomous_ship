b8db42ff40c9ea5df00ad9ff57a8309c1e279adb
LeeBeomHyeok <hyeok1531@gmail.com>
2020-08-11 11:11:16 +0900

    motor

--- forPC/pcMain.py
+++ forPC/pcMain.py
@@ -33,17 +33,42 @@ class Ship:
             # print(self.lidar.lidarDatas)
             self.publishMotor()
             rospy.sleep(1)
+    
+    def shipReward():
+        self.motorValue.rightMotor = 3
+        self.motorValue.leftMotor = 3
+
+    def shipStraight():
+        self.motorValue.rightMotor = 7
+        self.motorValue.leftMotor = 7
+
+    def ReturnToZero():
 
     def calculateMotorValue(self, gpsCoordination):
         xCoordDifference = gpsCoordination[0] - self.gps.latitude
         yCoordDifference = gpsCoordination[1] - self.gps.longitude
-        if self.lidar.result == 0:
-            if xCoordDifference > 1:
-                self.motorValue.rightMotor = 10
-                self.motorValue.leftMotor = 10
-        else:
-            if self.lidar.result == 1:
+        imuXrotation = Imu.get_x_rotation
+        imuYrotation = Imu.get_y_rotation
 
+        if self.lidar.result == 0:
+            if xCoordDifference < 1 and yCoordDifference < 1: #do not goal to destination
+                self.shipStraight()#???
+                
+        else if self.lidar.result == 1:
+            if xCoordDifference < 1 and yCoordDifference < 1:
+                if imuXrotation <= 0:
+                    while(imuXrotation <= 45):
+                        self.motorValue.leftMotor = 7
+                    while(xCoordDifference <= 1):  #will modify defaul value
+                        self.shipStraight()
+
+        else if self.lidar.result == 8:
+            if xCoordDifference < 0 and yCoordDifference < 0: #when 
+                if imuXerotation >= 0:
+                    while(imuXrotation <= -45): # -10 == xcoord???? i don't know
+                        self.motorValue.rightMotor = 7
+                    while(xCoordDifference <= 1):
+                        self.shipStraight()
 
 if __name__ == '__main__':
     try:
