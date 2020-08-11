975f010227f4086436a304a0b3d45374c845ceb9
LeeBeomHyeok <hyeok1531@gmail.com>
2020-08-12 00:21:49 +0900

    teest

--- forPC/hopping.py
+++ forPC/hopping.py
@@ -3,7 +3,7 @@ from handleGps import Gps
 import rospy
 import time
 class Hopping:
-    #gpsCoordinations = [[100, 10], [500, 300], [600, 100]]
+    gpsCoordinations = [[100, 10], [500, 300], [600, 100]]
 
     motorPub = rospy.Publisher('motor', motorValue, queue_size=10)
     motorValue = motorValue()
@@ -11,56 +11,13 @@ class Hopping:
     lidar = Lidar()
     imu = Imu()
     gps = Gps()
+
+    
+
     def motorStraight(self):
         self.motorValue.rightMotor = 6
         self.motorvalue.leftMotor = 6
         time.sleep(1)
-
-    def goToFirstDestination(self):
-        if gpsCoordination[0][0] -100 <= gps[1] <= gpsCoordination[0][0] +100 and
-         gpsCoordination[0][1] -100 <= gps[2] <= gpsCoordination[0][1] +100: 
-            self.motorValue.rightMotor = 6
-            time.sleep(1)
-            while(gpsCoordination[1][0] -100 <= gps[1] <= gpsCoordination[1][0] +100 and
-                gpsCoordination[1][1] -100 <= gps[2] <= gpsCoordination[1][1] +100)
-                self.motorStraight()
-            self.goToSecondDestination()
-
-
-    def goToSecondDestination(self):
-        if gpsCoordination[1][0] -100 <= gps[1] <= gpsCoordination[1][0] +100 and
-            gpsCoordination[1][1] -100 <= gps[2] <= gpsCoordination[1][1] +100:
-            self.motorValue.leftMotor = 7
-            time.sleep(3)
-
-            while(gpsCoordination[2][0] -100 <= gps[1] <= gpsCoordination[2][0] +100 and
-                gpsCoordination[2][1] -100 <= gps[2] <= gpsCoordination[2][1] +100)
-                self.motorStraight()
-            self.goToThirdDestination()
-
-    def goToThirdDestination(self):
-        if gpsCoordination[2][0] -100 <= gps[1] <= gpsCoordination[2][0] +100 and
-            gpsCoordination[2][1] -100 <= gps[2] <= gpsCoordination[2][1] +100:
-            self.motorValue.rightMotor = 6
-            time.sleep(3)
-
-            while(gpsCoordination[3][0] -100 <= gps[1] <= gpsCoordination[3][0] +100 and
-                gpsCoordination[3][1] -100 <= gps[2] <= gpsCoordination[3][1] +100)
-                self.motorStraight()
-            self.goToFourthDestination()
-    
-    def goToFourthDestination(self):
-        if gpsCoordination[2][0] -100 <= gps[1] <= gpsCoordination[2][0] +100 and
-            gpsCoordination[2][1] -100 <= gps[2] <= gpsCoordination[2][1] +100:
-            self.motorValue.leftMotor = 6
-            time.sleep(3)
-
-            while(gpsCoordination[3][0] -100 <= gps[1] <= gpsCoordination[3][0] +100 and
-                gpsCoordination[3][1] -100 <= gps[2] <= gpsCoordination[3][1] +100)
-                self.motorStraight()
-    
-
-
 
     def runSubscribers(self):
         self.lidar.handleLidar()
@@ -89,9 +46,48 @@ class Hopping:
             print(self.lidar.lidarData)
             self.publishMotor()
             rospy.sleep(1)
+
+    def returnBoolGps(self, gpsCoordinations, gps):
+        value = gpsCoordinations - gps
+        result = False
+        if value <= 0:
+            result = True
+        return result
+
 if __name__ == "__main__":
     try:
+        aa = Hopping
+
         
-        goToFirstDestination()
-        
+
+        while(True):
+            
+            aa.returnBoolGps(gpsCoordinations[][], gps[1])    
+
+            if  
+                
+             
+            
+            
+             #    yGps = gpsCoordinations[0][1] - gps[1]
+
+
     except:
+
+
+'''
+15750
+73270
+
+17300
+72240
+
+16
+
+
+
+
+
+17890
+76840
+'''
